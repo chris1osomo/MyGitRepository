68870bd9033ec0e00affc2f50ce394b9a092e8fa
Christopher Osomo <chris1osomo@gmail.com>
2022-09-13 06:31:23 -0600

    First Commit

---
+++ Colors.py
@@ -0,0 +1,8 @@
+from termcolor import colored
+print(colored("Hello, World!","blue"))
+print(colored("Hello, World!","yellow"))
+print(colored("Hello, World!","green"))
+print(colored("Hello, World!","red"))
+print(colored("Hello, World!","grey"))
+print(colored("Hello, World!","magenta"))
+print(colored("Hello, World!","cyan"))
